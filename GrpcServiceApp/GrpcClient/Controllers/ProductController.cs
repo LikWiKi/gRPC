f3fccdfc0f322655acb3391f4d39e743e3a70652
LiWiKi <89512540+LikWiKi@users.noreply.github.com>
2022-09-08 15:59:52 +0700

    DemoCRUD
    DemoCRUD

---
+++ GrpcServiceApp/GrpcClient/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceApp;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrpcClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ProductCRUD.ProductCRUDClient(channel);
+            var products = client.GetAll(new Empty());
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, float price, int stock, string description, int color, int size, DateTime dateCreated)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ProductCRUD.ProductCRUDClient(channel);
+
+            Empty res = client.Create(new Product()
+            {
+                 Name = name,
+                 Price = price,
+                 Stock = stock,
+                 Description = description,
+                 Color = color,
+                 Size = size,
+                 DateCreated = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.SpecifyKind(dateCreated, DateTimeKind.Utc))
+            });
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ GrpcServiceApp/GrpcServiceApp/Services/ProductService.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Core;
+using GrpcServiceApp.Data;
+
+namespace GrpcServiceApp.Services
+{
+    public class ProductService : ProductCRUD.ProductCRUDBase
+    {
+        private readonly AppDbContext _db;
+
+        public ProductService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public override Task<Products> GetAll(Empty requestData, ServerCallContext context)
+        {
+            Products responseData = new Products();
+            var query = from p in _db.Products
+                        select new Product()
+                        {
+                            Id = p.Id,
+                            Name = p.Name,
+                            Price = p.Price,
+                            Stock = p.Stock,
+                            Description = p.Description,
+                            Color = p.Color,
+                            Size = p.Size,
+                            DateCreated = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.SpecifyKind(p.DateCreated, DateTimeKind.Utc))
+                        };
+            responseData.Items.AddRange(query.ToArray());
+            return Task.FromResult(responseData);
+        }
+
+        public override Task<Empty> Create(Product requestData, ServerCallContext context)
+        {
+            _db.Products.Add(new Data.Product
+            {
+                Id = requestData.Id,
+                Name = requestData.Name,
+                Price = requestData.Price,
+                Stock = requestData.Stock,
+                Description = requestData.Description,
+                Color = requestData.Color,
+                Size = requestData.Size,
+                DateCreated = requestData.DateCreated.ToDateTime()
+            });
+            _db.SaveChanges();
+            return Task.FromResult(new Empty());
+        }
+    }
+}
